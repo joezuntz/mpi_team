025265a9c6caeddcd2f7ce729b64ec02a3cd1dec
joezuntz <joezuntz@googlemail.com>
2015-09-29 09:20:07 -0700

    bug fixes

--- mpi_psf.py
+++ mpi_psf.py
@@ -19,7 +19,7 @@ class PSFProcess(MPIProcess):
         for meds_file in meds_files:
             m = py3shape.i3meds.I3MEDS(meds_file, blacklist=self.options.blacklist)
             iobjs = np.arange(m.size)
-            chunks = np.array_split(iobjs, size)
+            chunks = np.array_split(iobjs, len(iobjs)//10)
             for chunk in chunks:
                 results.append((meds_file, chunk))
 
@@ -30,8 +30,9 @@ class PSFProcess(MPIProcess):
         filename, iobjs = tasks
         if filename!=self.filename:
             self.meds = py3shape.i3meds.I3MEDS(filename, blacklist=self.options.blacklist)
-
-        cat = self.meds.get_cat()
+            self.filename = filename
+        m=self.meds
+        cat = m.get_cat()
         results = [filename]
 
         for iobj in iobjs:
@@ -43,16 +44,17 @@ class PSFProcess(MPIProcess):
                 mom = psf.weighted_moments(weight_radius=10.)
                 e1 = mom.e1
                 e2 = mom.e2
-                e1_sky, e2_sky = m.convert_g_image2sky(iobj, iexp, options.stamp_size, e1, e2)
+                e1_sky, e2_sky = m.convert_g_image2sky(iobj, iexp, self.options.stamp_size, e1, e2)
                 ID = cat['id'][iobj]
                 results.append((ID, e1, e2, e1_sky, e2_sky))
-        return results
+        return [results]
 
     def write_output(self, results):
         filename = results[0]
         results = results[1:]
         cursor = self.connection.cursor()
         for result in results:
+            print result
             cursor.execute("insert into spteg_psf values (%s,%s,%s,%s,%s)", result)
         self.connection.commit()
         cursor.close()
@@ -60,14 +62,12 @@ class PSFProcess(MPIProcess):
     def master_setup(self):
         #connect to database
         self.options = py3shape.options.Options()
-        self.options.read()
         self.options.read(args["ini"])
         self.connection = desnerscdb.connect()
         self.meds_list = args['list']
 
     def slave_setup(self):
         self.options = py3shape.options.Options()
-        self.options.read()
         self.options.read(args["ini"])
         self.meds = None
         self.filename = ""
