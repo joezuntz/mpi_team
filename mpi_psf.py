8e37239172918a31fc69bab56dff07f1b8883d68
Joe Zuntz <joezuntz@googlemail.com>
2015-09-29 17:23:43 +0100

    add tile

--- mpi_psf.py
+++ mpi_psf.py
@@ -9,11 +9,25 @@ import desnerscdb
 from mpi_master_slave import MPIProcess
 
 
+tile_pattern = re.compile(r'DES[0-9][0-9][0-9][0-9][+-][0-9][0-9][0-9][0-9]')
+
+
+def find_tilename(name):
+    m = tile_pattern.search(name)
+    if m is None:
+        return "unknown"
+    return m.group()
+
+
+
 class PSFProcess(MPIProcess):
+    def find_completed(self):
+
+
     #Functions you must overwrite
     def make_tasks(self):
         results = []
-
+        completed_already = sel
         meds_files = [line.strip() for line in open(self.meds_list) if line.strip() and not line.strip().startswith('#')]
 
         for meds_file in meds_files:
@@ -46,16 +60,16 @@ class PSFProcess(MPIProcess):
                 e2 = mom.e2
                 e1_sky, e2_sky = m.convert_g_image2sky(iobj, iexp, self.options.stamp_size, e1, e2)
                 ID = cat['id'][iobj]
-                results.append((ID, e1, e2, e1_sky, e2_sky))
+                results.append((ID, iexp, e1, e2, e1_sky, e2_sky))
         return [results]
 
     def write_output(self, results):
         filename = results[0]
         results = results[1:]
         cursor = self.connection.cursor()
+        tile = find_tilename(filename)
         for result in results:
-            print result
-            cursor.execute("insert into spteg_psf values (%s,%s,%s,%s,%s)", result)
+            cursor.execute("insert into spteg_psf values ({0},%s,%s,%s,%s,%s,%s)".format(tile), result)
         self.connection.commit()
         cursor.close()
 
